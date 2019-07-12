4b9aa4a7e073763b88e5a404954c980b55cbfd21
Ariela Cohen <arielaades@gmail.com>
2019-07-12 13:58:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] != "" && " " 
+  if board[index] != "" && " " && nil
     return true
   else
     return false
